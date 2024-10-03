1c5ebc9221d920203ecfe7a08ced22b9d595c8d1
dgermani <dgermani@emeal.nttdata.com>
2024-10-03 14:10:45 +0200

    feat: added favorites grid

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { useGlobalState } from '../../context/GlobalState';
 import CharacterCard from '../CharacterCard/CharacterCard';
 import './Favorites.module.scss';
+import styles from './Favorites.module.scss';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Favorites: React.FC = () => {
   const { favorites } = useGlobalState();
 
   return (
     <div className="favorites">
-      <div className="character-list">
+      {/* <SearchBar searchTerm={searchTerm} onSearchChange={handleSearchChange} /> */}
+      <div>{favorites.length} RESULTS</div>
+      <div className={styles.characterList}>
         {favorites.length > 0 ? (
           favorites.map((character) => (
             <CharacterCard key={character.id} character={character} />
