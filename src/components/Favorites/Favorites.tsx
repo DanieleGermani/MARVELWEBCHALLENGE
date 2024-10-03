c23a08d6c92ea54a0b504da118a0ad358b20391a
dgermani <dgermani@emeal.nttdata.com>
2024-10-03 14:35:01 +0200

    feat:added search bar in favorite page

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGlobalState } from '../../context/GlobalState';
 import CharacterCard from '../CharacterCard/CharacterCard';
 import './Favorites.module.scss';
 import styles from './Favorites.module.scss';
 import SearchBar from '../SearchBar/SearchBar';
+import { ICharacter } from '../../models/character.model';
 
 const Favorites: React.FC = () => {
   const { favorites } = useGlobalState();
+  const [filteredFavorites, setFilteredFavorites] = useState<ICharacter[]>(favorites);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  
+  const handleSearchChange = (term: string) => {
+    setSearchTerm(term);
+    const filtered = favorites.filter((character) =>
+      character.name.toLowerCase().includes(term.toLowerCase())
+    );
+    setFilteredFavorites(filtered);
+  };
 
   return (
     <div className="favorites">
-      {/* <SearchBar searchTerm={searchTerm} onSearchChange={handleSearchChange} /> */}
-      <div>{favorites.length} RESULTS</div>
+      <SearchBar searchTerm={searchTerm} onSearchChange={handleSearchChange} />
+      <div>{filteredFavorites.length} RESULTS</div>
       <div className={styles.characterList}>
-        {favorites.length > 0 ? (
-          favorites.map((character) => (
+        {filteredFavorites.length > 0 ? (
+          filteredFavorites.map((character) => (
             <CharacterCard key={character.id} character={character} />
           ))
         ) : (
